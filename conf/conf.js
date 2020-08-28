238df5bff93288acb935473d000e9e13b0094e6f
sk.baithadiya <sk.baithadiya@gmail.com>
2020-08-28 14:00:35 +0700

    adding report

--- conf/conf.js
+++ conf/conf.js
@@ -12,4 +12,20 @@ exports.config = {
     // onPrepare: async () => {
     //     await browser.waitForAngularEnabled(false);
     // }
+    onPrepare: function(){
+        let HtmlReporter = require('protractor-beautiful-reporter');
+                     jasmine.getEnv().addReporter(new HtmlReporter({
+                        baseDirectory: 'reports',
+                        screenshotsSubfolder: 'screenshotsOnFailure',
+                        takeScreenShotsOnlyForFailedSpecs: true,
+                        jsonsSubfolder: 'jsonFiles',
+                        excludeSkippedSpecs: true,
+                        preserveDirectory: false,
+                        clientDefaults:{
+                        showTotalDurationIn: "header",
+                        totalDurationFormat: "h:m:s",
+                        gatherBrowserLogs: true
+                      },
+                     }).getJasmine2Reporter());
+        }
 }
